ecf357d622e498bcf97b3b9a6193e5996d417ec5
Praveen manchi <praveenmanchi@proton.me>
2023-11-22 00:07:02 +0530

    Tried again

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-// Importing necessary icons and components from Material-UI
 import SubdirectoryArrowLeftIcon from '@mui/icons-material/SubdirectoryArrowLeft';
-import { Grid, IconButton } from '@mui/material';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import Link from '@mui/material/Link';
-import Typography from '@mui/material/Typography';
+import { Grid, IconButton, Button, Dialog, DialogActions, DialogContent, Link, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import React, { useState } from 'react';
-import { inject } from '@vercel/analytics';
-
-// Importing CSS file and a custom component
 import './App.css';
 import QuestionBox from './components/QuestionBox/QuestionBox';
+import { inject } from '@vercel/analytics';
 
-// Previous questions data
 const previousQuestions = [
   {
     title: 'What is design thinking',
@@ -28,98 +18,88 @@ const previousQuestions = [
   },
 ];
 
-// Styling for the BootstrapDialog component
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  // Styles for Dialog content
   '& .MuiDialogContent-root': {
     padding: theme.spacing(2),
   },
-  // Styles for Dialog actions
   '& .MuiDialogActions-root': {
     padding: theme.spacing(1),
   },
-  // Styles for Dialog paper
   '& .MuiDialog-paper': {
-    maxWidth: '80%', // Adjust the width as needed
+    maxWidth: '80%',
     borderRadius: theme.spacing(2),
     paddingBottom: '25px',
   },
 }));
 
 function App() {
-  // State variables using React Hooks
   const [searchValue, setSearchValue] = useState('');
   const [open, setOpen] = useState(false);
   const [assistantResponse, setAssistantResponse] = useState('');
+  const [conversationHistory, setConversationHistory] = useState([]);
 
-  // Function to handle input change in search bar
   const handleInputChange = (e) => {
     setSearchValue(e.target.value);
   };
 
-  // Function to open the dialog
   const handleClickOpen = () => {
     setOpen(true);
   };
 
-  // Function to close the dialog
   const handleClose = () => {
     setOpen(false);
   };
 
-  // Function to handle click on search button
   const handleSearchButtonClick = () => {
-    fetchAssistantResponse(); // Call the function to fetch assistant response
+    fetchAssistantResponse();
     handleClickOpen();
   };
 
-  // Function to handle key press (specifically 'Enter') in the search bar
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
-      fetchAssistantResponse(); // Call the function to fetch assistant response
+      fetchAssistantResponse();
     }
   };
 
-  
-
-  // Function to fetch assistant response from OpenAI's API
   const fetchAssistantResponse = async () => {
     if (!searchValue.trim()) {
       return;
     }
-  
+
+    let updatedHistory = [...conversationHistory, { "role": "user", "content": searchValue }];
+
     try {
-      const response = await fetch('https://api.openai.com/v1/assistants', {
+      const response = await fetch('https://api.openai.com/v1/chat/completions', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': 'sk-Uugj3kr4wInXFvb4fPvT3BIbkFJ3tdnjzu5WBPZ789iyfVY', // Replace with your API key
+          'Authorization': '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', // Replace with your API key
         },
         body: JSON.stringify({
-          prompt: searchValue,
-          max_tokens: 100,
+          model: "gpt-3.5-turbo", // or "gpt-4" based on your preference
+          messages: updatedHistory,
         }),
       });
-  
+
       if (!response.ok) {
         throw new Error('Failed to fetch');
       }
-  
+
       const result = await response.json();
-      console.log('POST request result:', result);
-      setAssistantResponse(result.choices[0].text.trim());
+      const assistantMessage = result.choices[0].message.content;
+
+      setAssistantResponse(assistantMessage);
+      setConversationHistory(updatedHistory);
+
     } catch (error) {
-      console.error('Error making POST request:', error.message);
+      console.error('Error:', error.message);
     }
   };
-    // Call inject function to initiate analytics tracking
-    inject();
 
-  
-  // The return statement contains JSX for the application layout
+  inject();
+
   return (
     <>
-      {/* Main Grid container for the entire app */}
       <Grid
         container
         className="main-container"
@@ -129,37 +109,19 @@ function App() {
           height: '100%',
         }}
       >
-        {/* Grid item for the header */}
-        <Grid item xs={12}>
-          {/* Grid container for header content */}
-          <Grid container justifyContent={'space-between'}>
-            {/* Logo */}
-            <Grid item>
-              <img
-                src={`${process.env.PUBLIC_URL}/images/ux-ai-logo.svg`}
-                alt="i"
-              />
-            </Grid>
-            {/* Version */}
-            <Grid item className="version-style">
-              Beta V.02
-            </Grid>
-          </Grid>
-        </Grid>
-        {/* Grid item for the main content */}
+        {/* ... other Grid items and components ... */}
+
         <Grid
           item
           xs={12}
           sx={{ height: '90vh', display: 'flex', alignItems: 'center' }}
         >
-          {/* Grid container for the main content layout */}
           <Grid
             container
             flexDirection={'row'}
             justifyContent={'center'}
             gap={'40px'}
           >
-            {/* Search input container */}
             <Grid item className="input-field-container" xs={7}>
               <Grid
                 container
@@ -167,7 +129,6 @@ function App() {
                 alignItems={'center'}
                 justifyContent={'space-between'}
               >
-                {/* Search input field */}
                 <Grid item xs={7} md={10}>
                   <input
                     placeholder="Ask me about UX "
@@ -177,20 +138,13 @@ function App() {
                     onKeyPress={handleKeyPress}
                   />
                 </Grid>
-                {/* Search button */}
                 <Grid item className="enter-icon-container">
                   <IconButton onClick={handleSearchButtonClick}>
-                    <SubdirectoryArrowLeftIcon
-                      sx={{
-                        color: 'white',
-                        opacity: '0.5',
-                      }}
-                    />
+                    <SubdirectoryArrowLeftIcon sx={{ color: 'white', opacity: '0.5' }} />
                   </IconButton>
                 </Grid>
               </Grid>
             </Grid>
-            {/* Displaying previous questions */}
             <Grid item xs={8}>
               <Grid
                 container
@@ -253,15 +207,13 @@ function App() {
           </Link>
         </Grid>
       </Grid>
-      {/* BootstrapDialog component to display assistant response */}
+
       <BootstrapDialog
         onClose={handleClose}
         aria-labelledby="customized-dialog-title"
         open={open}
       >
-        {/* Dialog content */}
         <DialogContent>
-          {/* Grid container to display assistant response */}
           <Grid
             container
             style={{
@@ -270,13 +222,11 @@ function App() {
               borderRadius: '12px',
             }}
           >
-            {/* Displaying assistant's response */}
             <Typography gutterBottom sx={{ textAlign: 'center' }}>
               {assistantResponse || 'Ask me about UX...'}
             </Typography>
           </Grid>
         </DialogContent>
-        {/* DialogActions containing a button to ask again */}
         <DialogActions sx={{ display: 'flex', justifyContent: 'center' }}>
           <Button
             autoFocus
